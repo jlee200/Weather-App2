803b8d2ddd927e72732bc1589ffa4f030bec8ba0
jlee200 <justinlee@justins-MacBook-Pro-2.local>
2020-10-16 13:11:54 -0700

    revised code

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import {Image, ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import axios from 'axios';
 import background1 from './assets/background.png';
-import background2 from './assets/rainy background.png';
+import background2 from './assets/rainy_background.png';
 import cloud from './assets/cloud.png';
-import datafile from './datafile.js';
 import rain from './assets/rain.png';
 import sun from './assets/sun.png';
 import thun from './assets/thunder.png';
@@ -20,161 +20,243 @@ const WeatherAPI = axios.create({
   }
 });
 
+const PositionAPI = axios.create({
+  baseURL: 'http://api.positionstack.com/v1/',
+  timeout: 1000,
+});
+
+function getDayText(day) {
+  if (day === 0) return 'Sun';
+  if (day === 1) return 'Mon';
+  if (day === 2) return 'Tues';
+  if (day === 3) return 'Wed';
+  if (day === 4) return 'Thurs';
+  if (day === 5) return 'Fri';
+  if (day === 6) return 'Sat';
+  return '';
+}
+
+function getImage(feels) {
+  var str = feels.toLowerCase();
+  if (str.includes('sun') || str.includes('clear')) 
+    return sun;
+  if (str.includes('cloud')) 
+    return cloud;
+  if (str.includes('rain')) 
+    return rain;
+  if (str.includes('thunder')) 
+    return thun;
+  if (str.includes('wind')) 
+    return wind;
+  else 
+    return null;
+}
+
+function getBackground(feels) {
+  var str = feels.toLowerCase();
+  if(str.includes('sun') || str.includes('clear'))
+    return background1;
+  if(str.includes('rain') || str.includes('thunder') || str.includes('wind') || str.includes('cloud'))
+    return background2;
+  else 
+    return null;
+}
+
+function parseDataPOS(dataPOS){
+  return {
+    Lon = dataPOS[longitude],
+    Lat = dataPOS[latitude]
+  }
+};
+
+function parseData1(data1) {
+  const renderData = [];
+  var divide = data1.length / 5;
+  var start = divide / 2;
+
+  for (let i = 0; i < data1.length; i += divide) {
+    const mid = i + divide / 2;
+
+    const { timestamp_local, weather } = data1[mid];
+    const date = new Date(timestamp_local);
+    const dayOfWeek = getDayText(date.getDay());
+
+    let hi = data1[i].temp;
+    let low = data1[i].temp;
+    for (let j = i + 1; j < i + divide; j++) {
+      const { temp: jtemp } = data1[j];
+      hi = Math.max(jtemp, hi);
+      low = Math.min(jtemp, low);
+    }
+
+    const fHi = hi * (9 / 5) + 32;
+    const fLow = low * (9 / 5) + 32;
+    const weatherDescription = weather ? weather.description : '';
+
+    const wantedData = {
+      cHi: hi,
+      cLow: low,
+      dayOfWeek,
+      image: getImage(weatherDescription),
+      fHi,
+      fLow,
+    };
+
+    renderData.push(wantedData);
+  }
+  return renderData;
+}
+
+function parseData2(data) {
+  const { city_name, temp: temp2, weather: weather2 } = data;
+
+  let image2 = null;
+  if (weather2 && weather2.description) image2 = getImage(weather2.description);
+
+  let background = getBackground(weather2.description);
+  var farenheit2 = temp2 * (9 / 5) + 32;
+
+  return {
+    cityName: city_name,
+    image: image2,
+    f: farenheit2,
+    backimage: background
+  };
+}
+
 export default function App(){
   const [data1, setData1] = React.useState(null);
   const [data2, setData2] = React.useState(null);
+  const [value, setValue] = React.useState(null);
+  const [dataPOS, setDataPOS] = React.useState(null);
 
-  //React.useEffect( () => {
-    //WeatherAPI.get("https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=37.773972&lon=-122.431297").then ( data => {
-      //console.log("\n\nDATA1:", data);
-        //setData1(data);
-    //})
-    //WeatherAPI.get("https://weatherbit-v1-mashape.p.rapidapi.com/current?lat=37.773972&lon=-122.431297").then ( data => {
-      //console.log("\n\nDATA2:", data.data[0]);
-      //if(data.data[0])
-        //setData2(data.data[0]);
-    //})
-  //}, [])
+  const onChange = (text) => {
+    setValue(text)
+  }
+  
+  const onClickSearch = () => {
+    let input = 'https://api.positionstack.com/v1/forward'
+    const params = {
+      access_key: '2e57390d6ecdef89d24a18996f20aad6',
+      query: value
+    }  
+
+    PositionAPI.get(input, {params}).then((response) => {
+      if (response && response.data && response.data.results) {
+        setDataPOS(parseDataPOS(response.data.results));
+      }
+    });
+  }
+  
+  React.useEffect(() => {
+    let input = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly'
+    const {Lon, Lat} = dataPOS
+    const params = {
+      lat = Lat,
+      lon = Lon
+    }
+    WeatherAPI.get(input, {params}).then((response) => {
+      if (response && response.data && response.data.data) {
+        setData1(parseData1(response.data.data));
+      }
+    });
+  }, []);
 
   React.useEffect(() => {
-    const data = [
-      {
-      "rh":64,
-      "pod":"d",
-      "lon":-122.43,
-      "pres":1010.5,
-      "timezone":"America/Los_Angeles",
-      "ob_time":"2020-09-19 20:20",
-      "country_code":"US",
-      "clouds":75,
-      "ts":1600546800,
-      "solar_rad":561.8,
-      "state_code":"CA",
-      "city_name":"San Francisco",
-      "wind_spd":2.57,
-      "wind_cdir_full":"east",
-      "wind_cdir":"E",
-      "slp":1011.1,
-      "vis":5,
-      "h_angle":0,
-      "sunset":"02:10",
-      "dni":895.51,
-      "dewpt":15.5,
-      "snow":0,
-      "uv":4.01071,
-      "precip":0,
-      "wind_dir":80,
-      "sunrise":"13:56",
-      "ghi":803.44,
-      "dhi":114.49,
-      "aqi":99,
-      "lat":37.77,
-      "weather":{
-      "icon":"c02d",
-      "code":801,
-      "description":"Few clouds"
-      },
-      "datetime":"2020-09-19:20",
-      "temp":22.7,
-      "station":"3189P",
-      "elev_angle":51.03,
-      "app_temp":22.7,
-      }
-      ]
-    setData2(data);
-    setData1(datafile);
-  }, [])
-
-
-  if(!data1){
-    return(
-      <View><Text>Loading...</Text></View>
-    )
-  }
-
-  if(!data2){
-    return(
-      <View><Text>Loading...</Text></View>
-    )
-  }
-
-  //<View style={styles.container}>
-    //<ImageBackground source={background1} style={styles.background1}>
-    //</ImageBackground>
-  //</View>
-
-  
-  //Data1 code
-  const {temp: temp1, weather: weather1} = data1[i]; 
-
-  var divide = (data1.length/5);
-  var start = divide/2;
-  let image = null;
-  for(var i = start; i < data1.length; i+divide)
-    if(weather1 && weather1.description)
-      image = getImage(weather1.description);
-  
-  var x = 0; 
-  var Fhigh, Flow, Chigh, Clow = [0, 0, 0, 0, 0];
-
-  for(var k = 0; k < data1.length; k+divide) {
-    for(var l = k; l < k+divide; l++) {
-      Chigh[x] = temp1;
-      Clow[x] = temp1;
-      if(temp < Clow[x])
-        Clow[x] = temp1;
-      if(temp > Chigh[x])
-        Chigh[x] = temp1;
+    let input = 'https://weatherbit-v1-mashape.p.rapidapi.com/current'
+    const {Lon, Lat} = dataPOS
+    const params = {
+      lat = Lat,
+      lon = Lon
     }
-    x++;
+    WeatherAPI.get(input, {params}).then((response) => {
+      if (
+        response &&
+        response.data &&
+        response.data.data &&
+        response.data.data.length > 0
+      ) {
+        setData2(parseData2(response.data.data[0]));
+      }
+    });
+  }, []);
+
+  if(!data1 || !data2){
+    return(
+      <View><Text>Loading...</Text></View>
+    )
   }
 
-  for(x = 0; x < 5; x++) {
-    Fhigh[x] = ((Chi[x] * (9/5)) + 32);
-    Flow[x] = ((Clow[x] * (9/5)) + 32);
-  }
-  
-  //Data2 code
-  const {city_name, temp: temp2, weather: weather2} = data2[0];
-
-  let image2 = null;
-  if(weather2 && weather2.description)
-      image2 = getImage(weather2.description);
-  function getImage(feels){
-    var str = feels.toLowerCase();
-    if(str.includes('sun') || str.includes('clear'))
-      return sun;
-    if(str.includes('cloud'))
-      return cloud;
-    if(str.includes('rain'))
-      return rain;
-    if(str.includes('thunder'))
-      return thun;
-    if(str.includes('wind'))
-      return wind;
-    else
-      return null;
-  }
-
-  var farenheit2 = ((temp2 * (9/5)) + 32);
+  const{cityName, f, image, backimage} = data2; 
 
   return(
-  <View style={{display: 'flex', flexDirection: 'row'}}>
-    <Text>{city_name} {"\n"}Currently: {farenheit2}°F </Text>
-    {image && <Image source={image2} style={{width: 100, height: 50}}/>}
+    <ImageBackground source={backimage} 
+      style={{
+        flex: 1,
+        objectFit: 'cover',
+        width: '100vw',
+        height: '100vh'
+     }}>
+    <View>
+      <TextInput
+          onChangeText={onChange}
+          onSubmitEditing={onClickSearch}
+          value={value}
+      />
+
+      <TouchableOpacity 
+          onPress={onClickSearch}>
+          <Text>Go</Text>
+      </TouchableOpacity>
+    </View>
+
+    <View
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        padding: '5vw',
+        width: '100vw',
+        height: '100vh',
+    }}>
+
+    <Text style={styles.data2}>{cityName}</Text>
+    <Text style={styles.data2}>Currently: {Math.round(f)}°F</Text>
+    {image && <Image source={image} style={{ width: 100, height: 50, marginLeft: -20}} />}
+
+    <View style={{ display: 'flex', flexDirection: 'row' }}>
+      {data1.map((data) => {
+        const { cHi, cLow, dayOfWeek, fHi, fLow, image } = data;
+        return (
+          <View
+            key={dayOfWeek}
+            style={{
+              padding: '20px',
+              marginRight: '30px',
+            }}>
+            <Text style={styles.data1}>{dayOfWeek}</Text>
+            {image && (
+              <Image source={image} style={{ width: 80, height: 50, marginLeft: -12, marginBottom: 10 }} />
+            )}
+            <Text style={styles.data1}>Hi: {Math.round(fHi)}°F</Text>
+            <Text style={styles.data1}>Lo: {Math.round(fLow)}°F</Text>
+            <Text style={styles.data1}>Hi: {Math.round(cHi)}°C</Text>
+            <Text style={styles.data1}>Lo: {Math.round(cLow)}°C</Text>
+          </View>
+        );
+      })}
+    </View>
   </View>
-  )
-};
-
+  </ImageBackground>
+);
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column"
+  data1: {
+    marginBottom: 5
   },
-  background1: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
-  },
-});
-
+  data2: {
+    marginBottom: 5,
+    marginLeft: -30
+  }
+})
